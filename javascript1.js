05b33479efcee3763e2c6cc512efc0c8fe37bb3a
Saptarshi Roy <roysaptarshi093@gmail.com>
2020-07-29 12:23:00 +0530

    added new file

---
+++ javascript1.js
@@ -0,0 +1,7 @@
+var ourname = "freecode camp";
+var ourname ="hello, our name is " + ourname + ",how r u ?";
+
+var myname= "sap"
+var mystr = "my name is " +myname + " and i am well!";
+
+console.log(mystr)
