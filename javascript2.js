c14d00add9994cd283ded7ecf3d5e2db2407818c
Saptarshi Roy <roysaptarshi093@gmail.com>
2020-07-29 12:44:36 +0530

    find length of a string

---
+++ javascript2.js
@@ -0,0 +1,10 @@
+var firstnamelength = 0;
+var firstname ="ada";
+
+var firstnamelength = firstname.length;
+console.log(firstnamelength)
+
+var lastnamelength=0;
+var lastname="lovelace";
+var lastnamelength= lastname.length;
+console.log(lastnamelength)
